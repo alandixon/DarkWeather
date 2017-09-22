751c0536952a4f7c7b2532d1398a299683941ffd
AlanD <alandix@yahoo.com>
2017-09-22 22:38:21 +0100

    Added Sun

---
+++ DarkWeather/DarkWeather/Weather/Sun.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace DarkWeather.Weather
+{
+    public enum SunState
+    {
+        Undefined=0,
+        PreSunrise,
+        SunUp,
+        PostSunset
+    }
+
+    [DebuggerDisplay("SunState={SunState.ToString()} {SunPercent}%")]
+    public class Sun
+    {
+        public SunState SunState { get; set; }
+
+        public int SunPercent { get; set; }
+    }
+}
