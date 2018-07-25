f7cc6b481c0e651f2d53c5546e0b2ff73f939a9f
AlanD <alandix@yahoo.com>
2018-07-25 09:16:26 +0100

    Fixed .Net standard migration issues. Added PrecipitationAccumulation to dB. DarkSky 3.3.0 addition (I think). Fixed AccountStore creation problem.

--- DarkWeather/DarkWeather/Weather/AbsoluteTimeHourDataPoint.cs
+++ DarkWeather/DarkWeather/Weather/AbsoluteTimeHourDataPoint.cs
@@ -13,6 +13,7 @@ namespace DarkWeather.Weather
         public DateTime Time { get; set; }
         public DateTime LocalTime { get; set; }
         public float PrecipitationIntensity { get; set; }
+        public float PrecipitationAccumulation { get; set; }
         public float PrecipitationProbability { get; set; }
 
         // According to DarkSky, this can be one of "rain", "snow", or "sleet" or undefined if precipIntensity is zero
